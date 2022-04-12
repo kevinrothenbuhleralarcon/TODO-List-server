835df0c9c1409730cd0f13f3fe40d25d9c9f6761
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2022-04-12 16:35:44 +0200

    Add api call for getting a specific todo

--- public/scripts/todoApi.js
+++ public/scripts/todoApi.js
@@ -12,30 +12,34 @@ export default class TodoApi {
      * @returns {Promise<{connected: boolean, value: object}>} boolean - true if connected; value - Json object of the response
      */
     async login(data) {
-        const response = await fetch("/api/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
-        if(response.ok) {
-            const value = await response.json()
-            return {
-                connected: true,
-                value: value
-            }
-        } else {
-            const value = await response.text()
-            return {
-                connected: false,
-                value: value
-            }
+        try {
+            const response = await fetch("/api/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            if(response.ok) {
+                const value = await response.json()
+                return {
+                    connected: true,
+                    value: value
+                }
+            } else {
+                const value = await response.text()
+                return {
+                    connected: false,
+                    value: value
+                }
+            }        
+        } catch (err) {
+            throw (err)
         }        
     }
 
     /**
-     * 
+     * API call for account registration
      * @param {Object} data
      * @param {String} data.username 
      * @param {String} data.email 
@@ -43,36 +47,68 @@ export default class TodoApi {
      * @returns {Promise<{connected: boolean, value: object}>} boolean - true if connected; value - Json object of the response
      */
     async register(data) {
-        const response = await fetch("/api/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
-        if(response.ok) {
-            const value = await response.json()
-            return {
-                connected: true,
-                value: value
-            }
-        } else {
-            const value = await response.text()
-            return {
-                connected: false,
-                value: value
-            }
+        try {
+            const response = await fetch("/api/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            if(response.ok) {
+                const value = await response.json()
+                return {
+                    connected: true,
+                    value: value
+                }
+            } else {
+                const value = await response.text()
+                return {
+                    connected: false,
+                    value: value
+                }
+            }        
+        } catch (err) {
+            throw (err)
         }        
     }
 
+    /**
+     * API call for getting the list of todo for the connected user
+     * @returns {Promise<Object>} Json object of the response (an array of Todo) or the error message from the API
+     */
     async getTodoList() {
-        const response = await fetch("/api/todoList") 
-        if (response.ok) {
-            const data = await response.json()
-            return data.todos
-        } else {
-            const data = await response.text()
-            throw (data)
+        try {
+            const response = await fetch("/api/todoList") 
+            if (response.ok) {
+                const data = await response.json()
+                return data.todos
+            } else {
+                const data = await response.text()
+                throw (data)
+            }
+        } catch(err) {
+            throw(err)
         }
+        
+    }
+
+    /**
+     * API call for getting a specific todo belonging to the connected user
+     * @param {*} id 
+     */
+    async getTodo(id) {
+        try {
+            const response = await fetch(`/api/todo?id=${id}`)
+            if(response.ok) {
+                const data = await response.json()
+                return data.todo
+            } else {
+                const data = await response.text()
+                throw(data)
+            }
+        } catch (err) {
+            throw (err)
+        }        
     }
 }
