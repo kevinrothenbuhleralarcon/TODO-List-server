7eb6355cc2aa89b0a734672c3ecb8b5fa8287975
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2022-04-21 16:28:36 +0200

    Update the task detail html and add the logic for the todoDetail page

--- public/scripts/views/todo/detail/todoDetail.js
+++ public/scripts/views/todo/detail/todoDetail.js
@@ -3,6 +3,7 @@
 import TodoApi from "../../../todoApi.js"
 import AbstractView from "../../abstractView.js"
 import Todo from "../../../../model/todo.js"
+import Task from "../../../../model/task.js"
 
 export default class TodoUpdate extends AbstractView {
 
@@ -31,7 +32,7 @@ export default class TodoUpdate extends AbstractView {
         } else {
             super(`Edit todo ${id}`)
         }
-        this.#todoId = id
+        this.#todoId = Number(id)
         this.#todoApi = todoApi
         this.#emptyTaskTemplate = null
         this.#errorDiv = null
@@ -57,44 +58,103 @@ export default class TodoUpdate extends AbstractView {
      * @returns 
      */
     async executeViewScript(router) {
+        /** Add the action of the add task button */
         document.querySelector(".task-add-btn").addEventListener("click", (e) => {
             e.preventDefault()
             this.#addTask()
         })
+
         /** @type {HTMLButtonElement} */
         const btnSubmit = document.querySelector("#submit")
-        btnSubmit.addEventListener("click", (e) => {
+        btnSubmit.addEventListener("click", async (e) => {
             e.preventDefault()
+
             this.#cleanErrorMessage()
-            this.#validateForm(document.querySelector("#todo"))
+
+            /** Get the form data and validate them */
+            const form = document.querySelector("#todo")
+            /** @type {HTMLInputElement} */
+            const title = form.querySelector("#todoTitle")
+            /** @type {HTMLDivElement[]} */
+            const tasksNodes = [...form.querySelectorAll(".task-content")]
+            const errors = this.#validateForm(title, tasksNodes)
+            if (errors.length > 0) return this.#displayError(errors)
+
+            /** Create the todo to be sent to the server */
+            const createdAt = document.querySelector(".form-content").getAttribute("created-at")
+            /** @type {Todo} */
+            const todo = new Todo(
+                this.#todoId,
+                title.value,
+                createdAt === null ? new Date() : new Date(createdAt),
+                new Date(),
+                tasksNodes.map(task => {
+                    const deadline = task.querySelector(".deadline").value
+                    return new Task(
+                        Number(task.getAttribute("id")),
+                        task.querySelector(".description").value,
+                        task.querySelector(".status").checked,
+                        deadline === "" ? null : new Date(deadline),
+                        this.#todoId
+                    )
+                })
+            )    
+            
+            /** Send to todo to the api for insertion or update and redirect if or or display error message */
+            try {
+                let result
+                if (this.#todoId === null) {
+                    result = await this.#todoApi.addTodo(todo)
+                } else {
+                    result = await this.#todoApi.updateTodo(todo)
+                }
+                if (result.ok) {
+                    router("/")
+                } else {
+                    this.#displayError([result.value])
+                }
+            } catch (err) {
+                console.log(err)
+            }                     
         })
 
+        /** Set the title of the body and load the todo */
         if (this.#todoId === null) {
             document.querySelector("h1").innerHTML = "New todo"
         } else {
             document.querySelector("h1").innerHTML = `Edit todo ${this.#todoId}`
             try {
-                await this.#loadTodos()
+                await this.#loadTodo()
+
+                /** Add a delete button to delete an existing todo */
                 /** @type {HTMLButtonElement} */
                 const deleteButton = document.createElement("button")
                 deleteButton.innerHTML = "Delete"
-                deleteButton.addEventListener("click", (e) => {
+                deleteButton.addEventListener("click", async (e) => {
                     e.preventDefault()
-                    /** TODO */
-                    console.log("Delete")
+                    const result = await this.#todoApi.deleteTodo(this.#todoId)
+                    if (result.ok) {
+                        router("/")
+                    } else {
+                        this.#displayError([result.value])
+                    }
                 })
-            document.querySelector(".todo-form-button").append(deleteButton)
+                document.querySelector(".todo-form-button").append(deleteButton)
             } catch (err) {
                 return 
             }            
         }
     }    
 
-    async #loadTodos() {
+    /**
+     * Get the todo from the server and populate a new task Div
+     */
+    async #loadTodo() {
         try {
             const todo = await this.#todoApi.getTodo(this.#todoId)
             if (todo === null) return
             document.querySelector("#todoTitle").value = todo.title
+            document.querySelector(".form-content").setAttribute("created-at", todo.createdAt)
             for (const task of todo.tasks) {
                 /** @type {?HTMLDivElement} */
                 const taskNode = await this.#addTask()
@@ -109,10 +169,13 @@ export default class TodoUpdate extends AbstractView {
             }
         } catch(err) {
             console.log(err)
-            return
         }        
     }
 
+    /**
+     * Add a new task Div element and return it
+     * @returns {Promise<?HTMLDivElement>}
+     */
     async #addTask() {
         if (this.#emptyTaskTemplate === null) {
             try {
@@ -148,6 +211,10 @@ export default class TodoUpdate extends AbstractView {
         return taskNode
     }
 
+    /**
+     * Load the task template
+     * @returns {Promise<HTMLDivElement>}
+     */
     async #loadEmptyTaskTemplate() {
         try {
             const html = await fetch("./static/scripts/views/todo/detail/taskDetail.html").then(response => response.text())
@@ -172,25 +239,42 @@ export default class TodoUpdate extends AbstractView {
 
     /**
      * Validate the form and return an array of errors
-     * @param {HTMLFormElement} form
+     * @param {HTMLInputElement} title 
+     * @param {NodeListOf<HTMLDivElement>} tasksNodes
      * @returns {String[]} errors
      */
-    #validateForm(form) {
-
-        console.log(form.querySelector("#todoTitle").value)
-        const tasksNodes = form.querySelectorAll(".task-content")
-        console.log(tasksNodes)
-        /*const username = this.#form.username
-
-        const password = this.#form.password
-
+    #validateForm(title, tasksNodes) {        
+        /** @type {String[]} */
         const errors = []
-        if (username.validity.valueMissing) {
-            errors.push("Username cannot be empty")
+
+        if (title.validity.valueMissing) errors.push("Title cannot be empty")
+        if (tasksNodes.length < 1) {
+            errors.push("At least one task is needed")
+        } else {
+            let descriptionEmpty = false
+            tasksNodes.forEach(task => {
+                if (task.querySelector(".description").validity.valueMissing) {
+                    descriptionEmpty = true
+                    return
+                }
+            })
+            if (descriptionEmpty) errors.push("Description cannot be empty")
         }
-        if(password.validity.valueMissing) {
-            errors.push("Password cannot be empty")
-        }
-        return errors*/
+        return errors
+    }
+
+    /**
+     * Display an error on the form
+     * @param {String[]} errors 
+     */
+     #displayError(errors) {
+        /** @type {HTMLDivElement} */
+        const formContent = document.querySelector(".form-content")
+        this.#errorDiv = document.createElement("div")
+        this.#errorDiv.classList.add("error")
+        errors.forEach(error => {
+            this.#errorDiv.innerHTML += error + "<br>"
+        })        
+        formContent.insertBefore(this.#errorDiv, formContent.firstChild)
     }
 }
